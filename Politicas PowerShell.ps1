0e6e20b9b3bd226b8b4a98b1b1ad4d203ccc8ca8
Rodrigo Galvão Pedro Pereira <rgalvao93@outlook.com>
2025-01-13 00:33:15 -0300

    Correção
    Correção do nome do arquivo e dos comentários dos arquivos.

---
+++ Politicas PowerShell.ps1
@@ -0,0 +1,5 @@
+﻿# Define a política de execução como indefinida no escopo da máquina local.
+Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope LocalMachine
+
+# Define a política de execução como restrita no escopo do usuário atual.
+Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope CurrentUser
